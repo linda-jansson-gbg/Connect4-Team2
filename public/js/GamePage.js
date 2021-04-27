4c56edd029adb4c24bc34113e6767699ab0554bc
ironboy <thomas@nodehill.com>
2021-04-27 07:54:47 +0200

    Some basic oop in place + routing + event handling

---
+++ public/js/GamePage.js
@@ -0,0 +1,44 @@
+export default class GamePage {
+
+  constructor(app) {
+    this.app = app;
+    this.playerNames = [];
+
+    app.eventHandler.registerEvent(
+      'submit',
+      '.enter-names-form',
+      e => this.namesFormSubmit(e)
+    );
+  }
+
+  namesFormSubmit(e) {
+    e.preventDefault();
+    this.playerNames.push(
+      document.querySelector('input[name="player1name"]').value
+    );
+    this.playerNames.push(
+      document.querySelector('input[name="player2name"]').value
+    );
+    alert('Welcome ' + this.playerNames.join(', ') + '!');
+    // always re-render any change 
+    // by calling this.app.render()
+    this.app.render();
+  }
+
+  render() {
+    return /*html*/`
+      <main class="home__main">
+        <h1>Game</h1>
+        ${this.playerNames.length < 2 ? /*html*/`
+          <form class="enter-names-form">
+            Enter names:
+            <div><input name="player1name" type="text" placeholder="Player 1"></div>
+            <div><input name="player2name" type="text" placeholder="Player 2"></div>
+            <div><input type="submit" value="Start"></div>
+          </form>
+        ` : `Now render the board...`}
+      </main>
+    `
+  }
+
+}
